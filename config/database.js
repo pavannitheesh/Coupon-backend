8028dde531628aff60d133abb849e7247a27bf48
pavannitheesh <gpavannitheesh1@gmail.com>
2025-05-27 00:12:57 +0530

    feat(database): add PostgreSQL connection configuration

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+const pool = new Pool({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: {
+    require: true,
+  },
+});
+
+module.exports = pool;
